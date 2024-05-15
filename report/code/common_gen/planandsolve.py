6d96b5fe0ce528b8198adb9a4781394c2944cc6c
Tesa Robič Senečič <tr8824@hpc-login2.arnes.si>
2024-05-15 11:48:21 +0200

    restructure

---
+++ report/code/common_gen/planandsolve.py
@@ -0,0 +1,36 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+
+device = "cuda" # the device to load the model onto
+print("Setting device to " + device)
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+
+
+# Load the dataset
+dataset = load_dataset("allenai/common_gen", split="validation")
+print(f"Dataset loaded with {len(dataset)} entries.")
+#print(dataset[0])
+
+# Example of formatting prompts for a few examples
+counter = 0
+for example in dataset:  # Process just a few examples for demonstration
+    if counter < 3:
+        concepts = ", ".join(example['concepts'])
+        prompt = f"Generate only one sentence using all of the following concepts: {concepts}. 
+        Let's first understand the concepts and devise a plan how to create a sentence. Then, let's carry out the plan to solve the problem step by step."
+        
+
+        # This is the prompt you'd send to the model
+        #print(prompt)
+
+        input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
+        model.to(device)
+
+        generated_ids = model.generate(input_ids, max_new_tokens=1000, do_sample=True)
+        decoded = tokenizer.batch_decode(generated_ids)[0]
+        print(decoded)
+
+        counter += 1
+    else:
+        break
